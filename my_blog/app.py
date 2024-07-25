bc52f407d36e682224a5a88bed8b450334dde4e7
gopaljamnal <gopal.jamnal@gmail.com>
2024-07-25 17:40:03 +0100

    first commit with changes

--- my_blog/app.py
+++ my_blog/app.py
@@ -1,12 +1,99 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for, flash, jsonify
+from werkzeug.utils import secure_filename
+import os
+import json
+from datetime import datetime
 
 app = Flask(__name__)
+app.secret_key = 'supersecretkey'
+app.config['UPLOAD_FOLDER'] = 'static/uploads/'
+POSTS_FILE = 'posts.json'
+LIKES_FILE = 'likes.json'
 
+def ensure_upload_folder_exists():
+    if not os.path.exists(app.config['UPLOAD_FOLDER']):
+        os.makedirs(app.config['UPLOAD_FOLDER'])
+
+def load_posts():
+    if os.path.exists(POSTS_FILE):
+        with open(POSTS_FILE, 'r') as file:
+            try:
+                return json.load(file)
+            except json.JSONDecodeError:
+                return []
+    return []
+
+def save_posts(posts):
+    with open(POSTS_FILE, 'w') as file:
+        json.dump(posts, file)
+
+def load_likes():
+    if os.path.exists(LIKES_FILE):
+        with open(LIKES_FILE, 'r') as file:
+            try:
+                return json.load(file)
+            except json.JSONDecodeError:
+                return {}
+    return {}
+
+def save_likes(likes):
+    with open(LIKES_FILE, 'w') as file:
+        json.dump(likes, file)
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def index():
+    posts = load_posts()
+    return render_template('index.html', posts=posts)
 
+@app.route('/new', methods=['GET', 'POST'])
+def new_post():
+    if request.method == 'POST':
+        title = request.form['title']
+        content = request.form['content']
+        post_date = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        if 'image' in request.files:
+            image = request.files['image']
+            if image.filename != '':
+                filename = secure_filename(image.filename)
+                ensure_upload_folder_exists()
+                image.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+                image_url = url_for('static', filename=f'uploads/{filename}')
+            else:
+                image_url = None
+        else:
+            image_url = None
+
+        posts = load_posts()
+        posts.append({
+            'title': title,
+            'content': content,
+            'image_url': image_url,
+            'date': post_date,
+            'id': len(posts) + 1  # Simple ID for each post
+        })
+        save_posts(posts)
+
+        flash('New post created!', 'success')
+        return redirect(url_for('index'))
+    return render_template('new_post.html')
+
+@app.route('/like/<int:post_id>', methods=['POST'])
+def like_post(post_id):
+    likes = load_likes()
+    if str(post_id) in likes:
+        likes[str(post_id)] += 1
+    else:
+        likes[str(post_id)] = 1
+    save_likes(likes)
+    return jsonify({'likes': likes[str(post_id)]})
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
